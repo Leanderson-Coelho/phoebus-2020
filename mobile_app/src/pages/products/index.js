64bd2ab433d8bf12373bee563430a77f51815565
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-07-07 19:18:59 -0300

    feat: layout básico para listagem dos quadrinhos

--- mobile_app/src/pages/products/index.js
+++ mobile_app/src/pages/products/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+import ComicsService from '../../services/comicsService';
+import Style from './style';
+import CardComic from '../../components/CardComic';
 
 const Products = () => {
+  const [comics, setComics] = useState([]);
+  useEffect(() => {
+    loadComics();
+  }, []);
+
+  async function loadComics() {
+    try {
+      setComics(await ComicsService.loadComics());
+    } catch (err) {
+      console.log('falhar ao carregar comics');
+      console.error(err);
+    }
+  }
+
   return (
-    <View>
+    <View style={Style.container}>
       <Text>Página de produtos</Text>
+      <FlatList
+        style={Style.list}
+        data={comics}
+        renderItem={({ item, index }) => (
+          <CardComic comic={item} index={index} />
+        )}
+        keyExtractor={(item) => item.id + ''}
+      />
     </View>
   );
 };
---
+++ mobile_app/src/pages/products/style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../theme/colors';
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: colors.background,
+  },
+
+  list: {
+    padding: 10,
+  },
+});
