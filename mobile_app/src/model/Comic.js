1294dfe27040398f7c828d6ddfad545d31034a9b
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-07-07 19:19:43 -0300

    feat: modelo inicial para quadrinho

---
+++ mobile_app/src/model/Comic.js
@@ -0,0 +1,9 @@
+export class Comic {
+  constructor(id, title, date, price, thumbnail) {
+    this.id = id;
+    this.title = title;
+    this.date = date;
+    this.price = price;
+    this.thumbnail = thumbnail;
+  }
+}
