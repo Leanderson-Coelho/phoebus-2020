ead4766d7eb364be96ef0dc5095c44768eac661b
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-07-08 00:40:16 -0300

    feat: rotas com barra de menu no rodape

--- mobile_app/src/routes.js
+++ mobile_app/src/routes.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import colors from './theme/colors';
 import Products from './pages/products';
 
 const Routes = () => {
-  const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="products" component={Products} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        activeColor="green"
+        initialRouteName="products"
+        barStyle={{ backgroundColor: colors.backgroundBar }}>
+        <Tab.Screen
+          name="products"
+          component={Products}
+          options={{
+            tabBarLabel: 'Produtos',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="book-open-page-variant"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="cart"
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'Carrinho',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="cart-minus"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
 export default Routes;
