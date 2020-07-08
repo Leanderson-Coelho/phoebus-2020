266e488f6211f6fcdbe4c46a5561ecad25d2e2fc
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-07-08 11:12:40 -0300

    feat: modelo para quadrinho de detalhse

---
+++ mobile_app/src/model/CartItem.js
@@ -0,0 +1,10 @@
+export class CartItem {
+  constructor(id, title, thumbnail, price, quantity, subtotal) {
+    this.id = id;
+    this.title = title;
+    this.thumbnail = thumbnail;
+    this.price = price;
+    this.quantity = quantity;
+    this.subtotal = subtotal;
+  }
+}
---
+++ mobile_app/src/model/ComicDetail.js
@@ -0,0 +1,10 @@
+export class ComicDetail {
+  constructor(id, title, date, price, thumbnail, description) {
+    this.id = id;
+    this.title = title;
+    this.date = date;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.description = description;
+  }
+}
