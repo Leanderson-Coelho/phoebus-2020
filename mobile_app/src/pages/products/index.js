51c973228e3f22394c3c689f86c6517910436fbe
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2020-07-08 11:17:38 -0300

    refactor: removendo importacoes sem uso

--- mobile_app/src/pages/products/index.js
+++ mobile_app/src/pages/products/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList, Button } from 'react-native';
+import { View, FlatList } from 'react-native';
 
 import ComicsService from '../../services/comicsService';
 import Style from './style';
